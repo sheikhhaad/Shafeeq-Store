12e65be15c61ac940be5a8b24946cb93c629acac
sheikhhaad <sheikhhaad6@gmail.com>
2025-09-24 20:12:07 +0500

    done

--- supabse.js
+++ supabse.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kiqsxuzewbldnpyixlyd.supabase.co";
+const supabaseUrl = "https://yoepuzvkhjrlmedyqive.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpcXN4dXpld2JsZG5weWl4bHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NzM4NjIsImV4cCI6MjA3MzI0OTg2Mn0.a-e_ay7wmYm7n_4JtxRHLcpnf_6H6cA38A-fZhhR1Bs";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvZXB1enZraGpybG1lZHlxaXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MjI2NTMsImV4cCI6MjA3NDI5ODY1M30.eVwdcPSUu4u9LZQ1SgX-rRSoGVTKXuD7gNXyga7LBew";
 export const supabase = createClient(supabaseUrl, supabaseKey);
